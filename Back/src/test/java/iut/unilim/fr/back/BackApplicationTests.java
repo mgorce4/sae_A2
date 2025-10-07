22b1c721ca2bffb39646299efafed452e4eb828a
toast <159030724+mgorce4@users.noreply.github.com>
2025-10-07 14:13:26 +0200

    Initiate project

---
+++ Back/src/test/java/iut/unilim/fr/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package iut.unilim.fr.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
