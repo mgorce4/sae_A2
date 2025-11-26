0e492140d59bd49328d89e553f96614fd9b057d8
Noé Jacquet <noe.jacquet87@gmail.com>
2025-11-26 15:05:07 +0100

    add of the attribut ressources in Teacher

--- Back/src/main/java/iut/unilim/fr/back/users/Teacher.java
+++ Back/src/main/java/iut/unilim/fr/back/users/Teacher.java
@@ -1,13 +1,17 @@
 package iut.unilim.fr.back.users;
 
+import iut.unilim.fr.back.Ressource.Ressource;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teacher extends User {
 
-    //waiting for Ressource class
-    //private List<Ressource> ressources;
+    private List<Ressource> ressources;
 
     protected Teacher(String firstName, String lastName, String connexionId, String password, Institution institution) {
         super(firstName, lastName, connexionId, password, institution);
         this.status = 1;
-        //this.ressources = new ArrayList<Ressource>();
+        this.ressources = new ArrayList<>();
     }
 }
