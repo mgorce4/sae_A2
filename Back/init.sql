0f86de1eff8843a14f2edf2ff912112098bd64c0
toast <159030724+mgorce4@users.noreply.github.com>
2025-12-03 22:16:46 +0100

    Add AccessRight, Institution, and related entities with controllers and services

--- Back/init.sql
+++ Back/init.sql
@@ -2,35 +2,48 @@ CREATE TABLE INSTITUTION(
     id_institution SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     location TEXT NOT NULL
-)
-
-CREATE TABLE USER(
+);
+CREATE TABLE USERSYNCADIA(
     id_User SERIAL PRIMARY KEY,
-    accessRight IINT NOT NULL,
     firstname TEXT NOT NULL,
     lastname TEXT NOT NULL,
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     id_institution INT REFERENCES INSTITUTION(id_institution)
-)
+);
+CREATE TABLE TASK(
+    id_Task SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    firstDelivery DATE,
+    secondDelivery DATE,
+    id_User INT REFERENCES USERSYNCADIA(id_User)
+);
+
+
+CREATE TABLE ACCESSRIGHT(
+    accessRight INT NOT NULL,
+    id_User INT REFERENCES USERSYNCADIA(id_User)ON DELETE CASCADE,
+    PRIMARY KEY (accessRight, id_User)
+);
 
 CREATE TABLE TERMS(
     id_Terms SERIAL PRIMARY KEY,
     code TEXT NOT NULL
-)
+);
 
 CREATE TABLE HOURS_PER_STUDENT(
     id_HoursPerStudent SERIAL PRIMARY KEY,
     cm INT ,
     td INT,
     tp INT 
-)
+);
 
 CREATE TABLE PATH(
     id_Path SERIAL PRIMARY KEY,
     number INT NOT NULL,
     label TEXT NOT NULL
-)
+);
 
 CREATE TABLE UE(
     UE_Number SERIAL PRIMARY KEY,
@@ -38,13 +51,13 @@ CREATE TABLE UE(
     label TEXT NOT NULL,
     competenceLevel INT NOT NULL,
     id_Path INT REFERENCES PATH(id_Path)
-)
+);
 
 CREATE TABLE UE_COEFFICIENT(
     id_Coefficient SERIAL PRIMARY KEY,
     coefficient INT NOT NULL,
     UE_Number INT REFERENCES UE(UE_Number)
-)
+);
 
 CREATE TABLE RESSOURCE(
     id_Ressource SERIAL PRIMARY KEY,
@@ -54,21 +67,21 @@ CREATE TABLE RESSOURCE(
     id_Terms INT REFERENCES TERMS(id_Terms),
     id_HoursPerStudent INT REFERENCES HOURS_PER_STUDENT(id_HoursPerStudent),
     id_Coefficient INT REFERENCES UE_COEFFICIENT(id_Coefficient)
-)
+);
 
 CREATE TABLE PEDAGOGICAL_CONTENT(
     id_PedagogicalContent SERIAL PRIMARY KEY,
     cm TEXT,
     td TEXT,
     tp TEXT
-)
+);
 
 CREATE TABLE RESSOURCE_TRACKING(
     id_RessourceTracking SERIAL PRIMARY KEY,
     pedagogicalFeedback TEXT,
     studentFeedback TEXT,
     improvementSuggestions TEXT
-)
+);
 
 CREATE TABLE RESSOURCE_SHEET(
     id_RessourceSheet SERIAL PRIMARY KEY,
@@ -79,6 +92,71 @@ CREATE TABLE RESSOURCE_SHEET(
     id_Ressource INT REFERENCES RESSOURCE(id_Ressource),
     id_PedagogicalContent INT REFERENCES PEDAGOGICAL_CONTENT(id_PedagogicalContent),
     id_RessourceTracking INT REFERENCES RESSOURCE_TRACKING(id_RessourceTracking),
-    id_User INT REFERENCES USER(id_User)
-)
+    id_User INT REFERENCES USERSYNCADIA(id_User)
+);
 
+INSERT INTO INSTITUTION (name, location) VALUES
+('IUT du Limousin', 'Limoges'),
+('IUT de Guéret', 'Guéret'),
+('IUT de Brive', 'Brive');
+
+INSERT INTO USERSYNCADIA ( firstname, lastname, username, password, id_institution) VALUES
+( 'Anais', 'Poursat', 'apoursat', 'Password123', 1),
+( 'Tux', 'Linus', 'tlinus', 'TuxLePlusCool_1991_B)', 2),
+('Thomas', 'Hugel', 'thugel', 'FanDeTux:)', 2),
+('Max', 'Gorce', 'mgorce', 'SlayKing', 3);
+
+INSERT INTO TASK (name, description, firstDelivery, secondDelivery, id_User) VALUES
+('remplir fiche ressource', 'vous devez remplir la fiche ressource pour chaque ressource pédagogique que vous créez', '2026-01-30', '2026-07-15', 1),
+('mettre à jour fiche ressource', 'vous devez mettre à jour la fiche ressource pour chaque ressource pédagogique que vous modifiez', '2026-01-30', '2026-08-15', 3);
+
+INSERT INTO ACCESSRIGHT (accessRight, id_User) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(3, 1),
+(4, 3);
+
+INSERT INTO TERMS(code) VALUES
+('NGCC'),
+('NGCC'),
+('NGCC');
+
+INSERT INTO HOURS_PER_STUDENT (cm, td, tp) VALUES
+(6, 10, 20),
+(4, 10, 16),
+(10, 8, 10);
+
+INSERT INTO PATH(number, label) VALUES
+(1, 'BUT INFORMATIQUE parcours réalisation d''applications : conception, développement, validation');
+
+INSERT INTO UE(EuApogeeCode, label, competenceLevel, id_Path) VALUES
+('TIN0101U', 'UE1.1 - Réaliser',1, 1),
+('TIN0102U', 'UE1.2 - Optimiser',1, 1),
+('TIN0103U', 'UE1.3 - Administrer',1, 1);
+
+INSERT INTO UE_COEFFICIENT(coefficient, UE_Number) VALUES
+(3, 1),
+(4, 2),
+(2, 3);
+
+INSERT INTO RESSOURCE(apogeeCode, label, diffMultiCompetences, id_Terms, id_HoursPerStudent, id_Coefficient) VALUES
+('TIN01A1M', 'R1.01 Initiation au développement', FALSE, 1, 1, 1),
+('TIN01A2M', 'R1.02 Développement d''interfaces web', FALSE, 2, 2, 2),
+('TIN01A3M', 'R1.03 Introduction à l''architecture des ordinateurs', FALSE, 3, 3, 3);
+
+INSERT INTO PEDAGOGICAL_CONTENT(cm, td, tp) VALUES
+('1 Ennui ultime du python,2 pourquoi le prof il fait des dessins frr?,3 j''abandonne c''est trop là', '1 waw en vrai c''est ok de fou,2 euh ok c''est chelou mais ok ig,3 c''est quoi cette merde là laissez moi mourir', '1 hello world,2 bibliothèque des pleurs,3 putain c''est quoi pickle ;-;'),
+(NULL, '1 oww un petit chat,2 mmm redirections? c''est quoi ça?', '1 euh pourquoi le niveau il a triplé, 2 MAIS C''EST QUOI LE CSS DE MERDE,3 NON'),
+('1 waw les bits,2 mm pas cool les bits','1 pourquoi j''apprend ça même,2 aled','1 non laisse mo rentrer chez moi,2 non juste par pitié,3 pourquoi je m''inflige ça même');
+
+INSERT INTO RESSOURCE_TRACKING(pedagogicalFeedback, studentFeedback, improvementSuggestions) VALUES
+('Besoin de plus d''exemples','Trop facile','Ajouter des exercices'),
+('Parfait tel quel','Trop dur','Rien à ajouter'),
+('Bien structuré','Correct','Ajouter des vidéos explicatives');
+
+INSERT INTO RESSOURCE_SHEET(name, competence, SAE, year, id_Ressource, id_PedagogicalContent, id_RessourceTracking, id_User) VALUES
+('R1.01', 'Développement de base', 'SAE1', '2025-09-01', 1, 1, 1, 1),
+('R1.02', 'Développement web', 'SAE2', '2025-09-01', 2, 2, 2, 3),
+('R1.03', 'Architecture des ordinateurs', 'SAE3', '2025-09-01', 3, 3, 3, 3);
