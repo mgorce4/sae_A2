5d63fb85337127ea925aad0285c8a9b689df1207
Max GORCE <max.gorce@etu.unilim.fr>
2025-12-15 11:49:44 +0100

    correct corrupted file

--- Back/src/main/java/iut/unilim/fr/back/entity/NationalProgramObjective.java
+++ Back/src/main/java/iut/unilim/fr/back/entity/NationalProgramObjective.java
@@ -1,55 +1,55 @@
 package iut.unilim.fr.back.entity;
-}
-    }
-        this.ressourceSheet = ressourceSheet;
-    public void setRessourceSheet(RessourceSheet ressourceSheet) {
 
-    }
-        return ressourceSheet;
-    public RessourceSheet getRessourceSheet() {
-
-    }
-        this.content = content;
-    public void setContent(String content) {
-
-    }
-        return content;
-    public String getContent() {
-
-    }
-        this.idNationalProgramObjective = idNationalProgramObjective;
-    public void setIdNationalProgramObjective(Long idNationalProgramObjective) {
-
-    }
-        return idNationalProgramObjective;
-    public Long getIdNationalProgramObjective() {
-
-    }
-        this.ressourceSheet = ressourceSheet;
-        this.content = content;
-        this.idNationalProgramObjective = idNationalProgramObjective;
-    public NationalProgramObjective(Long idNationalProgramObjective, String content, RessourceSheet ressourceSheet) {
-
-    }
-    public NationalProgramObjective() {
-
-    private RessourceSheet ressourceSheet;
-    @JoinColumn(name = "id_RessourceSheet")
-    @ManyToOne(fetch = FetchType.LAZY)
-
-    private String content;
-    @Column(nullable = false, columnDefinition = "TEXT")
-
-    private Long idNationalProgramObjective;
-    @Column(name = "id_NationalProgramObjective")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
-public class NationalProgramObjective {
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name = "NATIONAL_PROGRAM_OBJECTIVE")
-@Entity
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity
+@Table(name = "NATIONAL_PROGRAM_OBJECTIVE")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class NationalProgramObjective {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_NationalProgramObjective")
+    private Long idNationalProgramObjective;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_RessourceSheet")
+    private RessourceSheet ressourceSheet;
+
+    public NationalProgramObjective() {
+    }
+
+    public NationalProgramObjective(Long idNationalProgramObjective, String content, RessourceSheet ressourceSheet) {
+        this.idNationalProgramObjective = idNationalProgramObjective;
+        this.content = content;
+        this.ressourceSheet = ressourceSheet;
+    }
+
+    public Long getIdNationalProgramObjective() {
+        return idNationalProgramObjective;
+    }
+
+    public void setIdNationalProgramObjective(Long idNationalProgramObjective) {
+        this.idNationalProgramObjective = idNationalProgramObjective;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public RessourceSheet getRessourceSheet() {
+        return ressourceSheet;
+    }
+
+    public void setRessourceSheet(RessourceSheet ressourceSheet) {
+        this.ressourceSheet = ressourceSheet;
+    }
+}
 
