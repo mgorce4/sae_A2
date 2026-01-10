eef6182cddabc1b20f8157f4d68fc3be3303209c
LESCHAEVE--LEMAIRE Nathaniel <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-01-10 13:30:22 +0100

    display of sae modification

--- Front/src/pages/form_mccc_sae.vue
+++ Front/src/pages/form_mccc_sae.vue
@@ -7,6 +7,7 @@
 
     let display_more_area = ref(false)
     let display_modify_area = ref(false)
+    let display_add_ue = ref(false)
 
     let modifySaeLabel = ref('')
     let modifySaeApogeeCode = ref('')
@@ -87,35 +88,50 @@
 
                 <form v-show="display_modify_area" method="post" v-on:submit.prevent="">
                     <div class="dark_bar"><p>Modification d'une SAÉ</p></div>
-                    <div class="panel_form_mccc container-fluid spa">
-                        <div class="left_side">
-                            <label for="label_sae_modify">Nom de la SAÉ :</label>
-                            <input id="label_sae_modify" type="text" v-model="modifySaeLabel" required>
+                    <div class="panel_form_mccc container-fluid">
+                        <div class="left_side container-fluid cfh spa">
+                            <div class="container-fluid spb">
+                                <label for="label_sae_modify">Nom de la SAÉ :</label>
+                                <input class="mccc_input" id="label_sae_modify" type="text" v-model="modifySaeLabel" required>
+                            </div>
+                            <div class="container-fluid spb">
+                                <label for="apogee_code_sae_modify">Code Apogée :</label>
+                                <input class="mccc_input" id="apogee_code_sae_modify" type="text" v-model="modifySaeApogeeCode" required>
+                            </div>
+                            <div class="container-fluid spb">
+                                <label for="hours_sae_modify">Nombre d'heures (formation initiale) :</label>
+                                <input class="mccc_input" id="hours_sae_modify" type="number" v-model="modifySaeHours" required>
+                            </div>
 
-                            <label for="apogee_code_sae_modify">Code Apogée :</label>
-                            <input id="apogee_code_sae_modify" type="text" v-model="modifySaeApogeeCode" required>
-
-                            <label for="hours_sae_modify">Nombre d'heures (formation initiale) :</label>
-                            <input id="hours_sae_modify" type="number" v-model="modifySaeHours" required>
+                            <div class="container-fluid spa">
+                                <input class="btn1" type="reset" value="Annuler" @click="display_modify_area = false; display_add_ue = false">
+                                <input class="btn1" type="submit" value="Sauvegarder" @click="saveModifiedSae">
+                            </div>
                         </div>
-                        <div class="right_side">
-                            <table class="ueCoefficient">
-                                <tr>
-                                    <td>U.E. affectée(s) : </td>
-                                    <th class="display_coef_label" v-for="(labelUe, index2) in ueCoefficients" v-bind:key="index2">{{ labelUe.ueLabel }}</th>
-                                </tr>
-                                <tr>
-                                    <td>Coefficient : </td>
-                                    <td class="display_coef_ue" v-for="(coefUe, index3) in ueCoefficients" v-bind:key="index3">{{ coefUe.coefficient }}</td>
-                                </tr>
-                                <tr>
-                                    <td></td>
-                                    <td class="button_more del_ue" v-for="(index4) in ueCoefficients" v-bind:key="index4" style="margin: 0 15%;">X</td>
-                                </tr>
-                            </table>
-                            <input class="btn1" type="reset" value="Annuler" @click="display_modify_area = false">
-                            <input class="btn1" type="submit" value="Sauvegarder" @click="saveModifiedSae">
-                        </div>
+                        <table class="ueCoefficient">
+                            <tr>
+                                <td>U.E. affectée(s) : </td>
+                                <th class="display_coef_label" v-for="(labelUe, index2) in ueCoefficients" v-bind:key="index2">{{ labelUe.ueLabel }}</th>
+                                <td v-show="display_modify_area && !display_add_ue" @click="display_add_ue = true">
+                                    <p class="button_more button_ue">+</p>
+                                </td>
+                                <th class="display_coef_label" v-show="display_modify_area && display_add_ue">...</th>
+                            </tr>
+                            <tr>
+                                <td>Coefficient : </td>
+                                <td class="display_coef_ue" v-for="(coefUe, index3) in ueCoefficients" v-bind:key="index3">{{ coefUe.coefficient }}</td>
+                                <td class="display_coef_ue" v-show="display_modify_area && display_add_ue">...</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td v-for="(index4) in ueCoefficients" v-bind:key="index4">
+                                    <p class="button_more button_ue">X</p>
+                                </td>
+                                <td v-show="display_modify_area && display_add_ue" @click="display_add_ue = false">
+                                    <p class="button_more button_ue">✓</p>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </form>
 
@@ -133,7 +149,7 @@
                                 <label>Nombre d'heures (formation initiale) : </label>
                                 <p class="mccc_input">{{ value.hours }}</p>
                             </div>
-                            <div class="container-fluid spb">
+                            <div class="container-fluid spa">
                                 <input id="btn_cancel_UE" class="btn1" type="reset" value="Supprimer">
                                 <input id="btn_save_UE" class="btn1" type="button" value="Modifier" @click="modifySae(value)">
                             </div>
@@ -166,7 +182,7 @@
 
 .left_side {
     width: 45%;
-    margin: 1vw 10vw 1vw 5vw;
+    margin: 1vw 5vw 1vw 2vw;
 }
 
 .mccc_input {
@@ -174,6 +190,7 @@
     background-color: var(--div-rect-background-color);
     border-radius: 0.5vw;
     padding: 0.5vw;
+    margin: 0.5vw 0;
     width: fit-content;
     min-width: 10vw;
     text-align: center;
@@ -183,31 +200,32 @@
 .ueCoefficient {
     border-collapse: separate;
     border-spacing: 0.6vw;
-    width: 100%;
-    margin-top: 1vw;
+    height: 3.5vw;
     text-align: center;
     align-items: center;
 }
 
-.display_coef_label {
+.display_coef_label, .display_coef_ue {
+    width: 2vw;
+    height: 1.25vw;
     border: 0.1vw solid var(--main-theme-terciary-color);
     background-color: var(--div-rect-background-color);
+    padding: 0.5vw;
+}
+
+.display_coef_label {
     border-top-left-radius: 0.8vw;
     border-top-right-radius: 0.8vw;
-    padding: 0.5vw;
 }
 
 .display_coef_ue {
-    border: 0.1vw solid var(--main-theme-terciary-color);
-    background-color: var(--div-rect-background-color);
     border-bottom-left-radius: 0.8vw;
     border-bottom-right-radius: 0.8vw;
-    padding: 0.5vw;
 }
 
-.del_ue {
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
+.button_ue {
+    width: 2vw !important;
+    height: 2vw !important;
+    margin: 0 auto;
 }
 </style>
