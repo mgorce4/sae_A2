922049631600de7561333876c614170bf70409c0
Max GORCE <max.gorce@etu.unilim.fr>
2026-03-13 14:43:48 +0100

    add link with smtp server

--- Back/src/main/java/iut/unilim/fr/back/BackApplication.java
+++ Back/src/main/java/iut/unilim/fr/back/BackApplication.java
@@ -2,12 +2,22 @@ package iut.unilim.fr.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class BackApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BackApplication.class, args);
-	}
+public class BackApplication implements CommandLineRunner {
+
+    @Autowired
+    private MailTestSender mailTestSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
     // Test pdf at localhost:8080/api/pdf/generate?resourceName=R1.01&userName=apoursat
     // Test csv at http://localhost:8080/api/csv/generate?resourceName=R1.01&userDepartment=INFORMATIQUE&userName=apoursat
 
