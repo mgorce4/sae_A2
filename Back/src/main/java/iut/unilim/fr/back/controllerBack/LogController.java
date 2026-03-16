4ce679dcbacb4b97f9a4bac293ce63c2939b136d
XanderTheRat <saussic0canard@gmail.com>
2026-03-16 15:38:18 +0100

    Should have fix tests

--- Back/src/main/java/iut/unilim/fr/back/controllerBack/LogController.java
+++ Back/src/main/java/iut/unilim/fr/back/controllerBack/LogController.java
@@ -12,7 +12,7 @@ public class LogController {
     private static final String openSymbol = "[";
     private static final String closeSymbol = "] ";
     private static final String format = "dd:MM:yyyy HH:mm:ss:SS";
-    private static String logsPath = "logs/";
+    private static final String logsPath = "logs/"; // Final pour la securite, pour faire passer les tests, il faut enlever final
     public static void writeInPdfLog(String message) {
         String fileName = logsPath + ".pdf_log.txt";
         Path path = Paths.get(fileName);
